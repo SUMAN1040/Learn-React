b7e7a99396eae185cdae45ca8fc0fc50ad6ec22c
Innox1040 <uemj2003@gmail.com>
2026-04-14 12:07:05 +0530

    feat: add TextForm component and enforce required propTypes for Navbar

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About"/>
-      {/* <Navbar /> */}
+      {/* <Navbar/> */}
+      <div className="container my-5">
+        <TextForm heading="Enter your text to analyze"/>
+      </div>
     </>
   );
 }
--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -49,7 +49,7 @@ export default function Navbar(props) {
   );
 }
 
-Navbar.propTypes = { title: PropTypes.string, aboutText: PropTypes.string };
+Navbar.propTypes = { title: PropTypes.string.isRequired, aboutText: PropTypes.string.isRequired };
 
 Navbar.defaultProps = {
   title: "Textutils",
